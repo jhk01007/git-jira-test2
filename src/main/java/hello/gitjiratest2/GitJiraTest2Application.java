80313ff494e37b6afce7a70b1bbc0eb0b78d0331
jhk01007 <jhk001007@naver.com>
2024-11-12 12:21:54 +0900

    fist commit

---
+++ src/main/java/hello/gitjiratest2/GitJiraTest2Application.java
@@ -0,0 +1,13 @@
+package hello.gitjiratest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitJiraTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitJiraTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/hello/gitjiratest2/GitJiraTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.gitjiratest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitJiraTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
